9542a33b4f857fec4eba970199973991cd56d22c
Kabir <sagithedeveloper@gmail.com>
2024-07-05 11:19:10 +0530

    Added the code

---
+++ 03_JS/S04_05-07-2024-Functions/Add.js
@@ -0,0 +1,7 @@
+var num1 = 100;
+
+var num2 = 200;
+
+var result = num1 + num2;
+
+console.log(result);
---
+++ 03_JS/S04_05-07-2024-Functions/function.js
@@ -0,0 +1,15 @@
+function add() {
+  var name = "Sagar";
+  console.log(name);
+}
+
+add();
+
+function sub() {
+  var x = 100;
+  var y = 200;
+  console.log(x, y);
+}
+
+sub();
+sub();
